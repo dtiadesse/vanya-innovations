94342d78a01d506d2753b2b9d0c85df25445ffc6
praneethabet <praneetha.ui6@gmail.com>
2020-06-05 15:58:50 -0700

    inital setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+{path:'header',component:HeaderComponent},
+{ path:'footer',component: FooterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
